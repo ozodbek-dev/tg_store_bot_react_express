cb557db59ccb14f292891a0e32d2e102df479812
ozodbek-dev <ozodbeksoftwaredev@gmail.com>
2023-03-28 16:18:21 +0500

    server files have been changed

--- server_bot/index.js
+++ server_bot/index.js
@@ -1,30 +1,73 @@
-import dotenv from 'dotenv'
-import TelegramBot from 'node-telegram-bot-api'
-dotenv.config();
-const webAppUrl = "https://google.com"
+import TelegramBot  from'node-telegram-bot-api';
+import express from 'express'
 
-const bot = new TelegramBot(process.env.TG_BOT_TOKEN, {polling:true});
+import cors from 'cors'
 
-bot.on("message", async(msg)=>{
+const token = process.env.TG_BOT_TOKEN;
+const webAppUrl = process.env.WEB_APP_URL;
+
+const bot = new TelegramBot(token, {polling: true});
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
-    if(text==="/start"){
-        await  bot.sendMessage(chatId,"Formaga o'tish", {
-            reply_markup:{
-                keyboard:[
-                    [{text:"Formani to'ldirish"}]
+
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ]
+            }
+        })
+
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
                 ]
             }
         })
     }
 
-    await  bot.sendMessage(chatId,"Quyidagi knopkani bosish orqali buyurtma bering 👇", {
-        reply_markup:{
-            inline_keyboard:[
-                [{text:"Buyurtma berish", web_app:{url:webAppUrl}}]
-            ]
+    if(msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
         }
-    })
+    }
+});
 
-
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
 })
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
